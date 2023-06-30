0ce6568259e15f7ce95d6ff5eadc5025761c03b3
Angel Carmona <64759952+Angel-Carmona@users.noreply.github.com>
2023-06-30 02:56:49 +0200

    Update dependences.php

--- src/dependences.php
+++ src/dependences.php
@@ -1,4 +1,9 @@
 <?php
+use Models\Database\Database;
+use Controllers\Views\View;
+use Controllers\Session\Session;
+
+require_once __DIR__ . '/../vendor/autoload.php';
 
 function myErrorHandler($errno, $errstr, $errfile, $errline)
 {
@@ -8,34 +13,29 @@ function myErrorHandler($errno, $errstr, $errfile, $errline)
         "errfile" => $errfile,
         "errline" => $errline
     ]));
-
     die(require_once __DIR__ . '/../assets/error_templates/error.php');
 }
 // A user-defined exception handler function
 function myException($exception)
 {
-    echo "<b>Error:</b> " . $exception->getMessage();
-    die();
+    die("<b>Error:</b> " . $exception->getMessage());
 }
-
 set_error_handler("myErrorHandler");
 set_exception_handler("myException");
-
-use Models\Database\Database;
-use Controllers\Views\View;
-use Controllers\Session\Session;
-
-require_once __DIR__ . '/../vendor/autoload.php';
 
 Session::start();
 
 if(!defined('DB')) {
-    define('DB', (new Database()));
+    define('DB', 
+        new Database()
+    );
 }
 
 $uri = explode('/', $_SERVER['REQUEST_URI']);
-$uri = end($uri);
-
 if(!defined('VIEW')) {
-    define('VIEW', (new View(($uri))));
+    define('VIEW', 
+        new View(
+            end($uri)
+        )
+    );
 }
